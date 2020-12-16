bb0fa763e0e23cf58816098b8a5c30df103ff9b1
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-12-16 16:24:54 +0900

    create build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -Ceu
+set -o pipefail
+
+cd $(dirname $0)
+
+mkdir -p bin
+
+for d in $(ls .); do
+    if [[ ! -d $d ]]; then
+        continue
+    fi
+    if [[ $d = bin ]] || [[ $d = var ]]; then
+        continue
+    fi
+    echo '################'
+    echo "# $d"
+    echo '################'
+    (
+        cd $d
+        make
+    )
+    if [[ -e $d/bin/$d ]] && ( [[ ! -e bin/$d ]] || ! cmp bin/$d $d/bin/$d 2>/dev/null ); then
+        cp -vp $d/bin/$d bin/$d
+    fi
+done
+
