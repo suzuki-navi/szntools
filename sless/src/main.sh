64e8d16e4b80cec096422ed219c8831ffd59c9be
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-01-03 19:41:36 +0900

    sless: コード整理

--- sless/src/main.sh
+++ sless/src/main.sh
@@ -2,63 +2,43 @@
 export SLESS_HOME=$SZNPACK_SOURCE_DIR
 
 option=
-if [ -t 1 ]; then
-    option="$option --color"
-fi
 
 declare -a files=()
-while [ $# -gt 0 ]; do
+while [[ $# -gt 0 ]]; do
     arg="$1"
-    if [ "$arg" = -v ]; then
+    if [[ "$arg" = -v ]]; then
         option="$option -v"
+    elif [[ "$arg" = -h ]]; then
+        option="$option -h"
     else
         files+=("$arg")
     fi
     shift
 done
 
-if [ -t 1 ]; then
+export TERMINAL_LINES=0
+export TERMINAL_COLS=0
+lesscmd="cat"
+if [[ -t 1 ]]; then
     tput lines >/dev/null 2>&1 && export TERMINAL_LINES=$(tput lines);
     tput cols  >/dev/null 2>&1 && export TERMINAL_COLS=$(tput cols);
-
-    if [ ${#files[@]} = 0 ]; then
-        perl $SLESS_HOME/format-wrapper.pl $option "$@" | less -SRX
-    elif [ ${#files[@]} = 1 ]; then
-        f=${files[0]}
-        if [ -d "$f" ]; then
-            ls -alF --color=always --time-style="+%Y-%m-%d %H:%M:%S" "$f" | less -SRX
-        else
-            perl $SLESS_HOME/format-wrapper.pl $option "$@" < "$f" | less -SRX
-        fi
-    else
-        for f in ${files[@]}; do
-            echo -e "\e[32m$f\e[m"
-            if [ -d "$f" ]; then
-                ls -alF --color=always --time-style="+%Y-%m-%d %H:%M:%S" "$f"
-            else
-                perl $SLESS_HOME/format-wrapper.pl $option "$@" < "$f"
-            fi
-        done | less -SRX
-    fi
-else
-    if [ ${#files[@]} = 0 ]; then
-        perl $SLESS_HOME/format-wrapper.pl $option "$@"
-    elif [ ${#files[@]} = 1 ]; then
-        f=${files[0]}
-        if [ -d "$f" ]; then
-            ls -alF --color=never --time-style="+%Y-%m-%d %H:%M:%S" "$f"
-        else
-            perl $SLESS_HOME/format-wrapper.pl $option "$@" < "$f"
-        fi
-    else
-        for f in ${files[@]}; do
-            echo -e "$f"
-            if [ -d "$f" ]; then
-                ls -alF --color=never --time-style="+%Y-%m-%d %H:%M:%S" "$f"
-            else
-                perl $SLESS_HOME/format-wrapper.pl $option "$@" < "$f"
-            fi
-        done
-    fi
+    option="$option --color"
+    lesscmd="less -i -SRX"
 fi
+
+if [[ ${#files[@]} = 0 ]]; then
+    bash $SLESS_HOME/object.sh $option
+elif [[ ${#files[@]} = 1 ]]; then
+    f="${files[0]}"
+    bash $SLESS_HOME/object.sh $option "$f"
+else
+    for f in ${files[@]}; do
+        if [[ -t 1 ]]; then
+            echo -e "\e[32m$f\e[m"
+        else
+            echo "$f"
+        fi
+        bash $SLESS_HOME/object.sh $option "$f"
+    done
+fi | $lesscmd
 
---
+++ sless/src/object.sh
@@ -0,0 +1,35 @@
+
+set -Ceu
+set -o pipefail
+
+format_wrapper_option=
+ls_option=
+color_flag=
+target_file=
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    if [[ "$arg" = -v ]]; then
+        format_wrapper_option="$format_wrapper_option -v"
+    elif [[ "$arg" = -h ]]; then
+        ls_option="$ls_option -h"
+    elif [[ "$arg" = --color ]]; then
+        color_flag=1
+        format_wrapper_option="$format_wrapper_option --color"
+    else
+        target_file=$1
+    fi
+    shift
+done
+
+if [[ "$target_file" = "" ]]; then
+    perl $SLESS_HOME/format-wrapper.pl $format_wrapper_option
+elif [[ -d "$target_file" ]]; then
+    if [[ -n "$color_flag" ]]; then
+        ls -alF $ls_option --color=always --time-style="+%Y-%m-%d %H:%M:%S" "$target_file"
+    else
+        ls -alF $ls_option --color=never --time-style="+%Y-%m-%d %H:%M:%S" "$target_file"
+    fi
+else
+    perl $SLESS_HOME/format-wrapper.pl $format_wrapper_option < "$target_file"
+fi
+
