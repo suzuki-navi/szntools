2992b919f372e6bb6c5c33041a76f796403f071b
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-01-03 20:15:21 +0900

    sless: ファイル名カラー表示

--- sless/src/main.sh
+++ sless/src/main.sh
@@ -19,11 +19,15 @@ done
 export TERMINAL_LINES=0
 export TERMINAL_COLS=0
 lesscmd="cat"
+filename_esc_1=""
+filename_esc_2=""
 if [[ -t 1 ]]; then
     tput lines >/dev/null 2>&1 && export TERMINAL_LINES=$(tput lines);
     tput cols  >/dev/null 2>&1 && export TERMINAL_COLS=$(tput cols);
     option="$option --color"
     lesscmd="less -i -SRX"
+    filename_esc_1="\e[32m"
+    filename_esc_2="\e[m"
 fi
 
 if [[ ${#files[@]} = 0 ]]; then
@@ -33,11 +37,7 @@ elif [[ ${#files[@]} = 1 ]]; then
     bash $SLESS_HOME/object.sh $option "$f"
 else
     for f in ${files[@]}; do
-        if [[ -t 1 ]]; then
-            echo -e "\e[32m$f\e[m"
-        else
-            echo "$f"
-        fi
+        echo -e "$filename_esc_1$f$filename_esc_2"
         bash $SLESS_HOME/object.sh $option "$f"
     done
 fi | $lesscmd
